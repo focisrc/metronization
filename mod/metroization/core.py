f0b857a20e66cfcc798bb59217aa8941b391e0da
Chi-kwan Chan <chanc@email.arizona.edu>
2018-08-18 01:15:49 -0700

    Implement visualization tool for the metroized data

--- mod/metroization/core.py
+++ mod/metroization/core.py
@@ -36,3 +36,48 @@ def metroize(img, mgrid=32, threshold=0.5):
     img = skeletonize(img > threshold)
     img = skeletonize(rebin(img, [mgrid, mgrid]) > 0)
     return img
+
+def metroplot(ax, img, **kwargs):
+    s0 = img.shape[0]
+    s1 = img.shape[1]
+    for i in range(s0):
+        for j in range(s1):
+            if not img[i,j]:
+                continue
+
+            c = 0
+            for ii in [i-1,i,i+1]:
+                for jj in [j-1,j,j+1]:
+                    if ii == i and jj == j:
+                        continue
+                    if ii < 0 or ii >= s0:
+                        continue
+                    if jj < 0 or jj >= s1:
+                        continue
+                    if img[ii,jj]:
+                        if ii != i and jj != j:
+                            if img[ii,j] or img[i,jj]:
+                                continue
+                        ax.plot([j+0.5,(jj-j)/2+j+0.5],
+                                [i+0.5,(ii-i)/2+i+0.5],
+                                color='k')
+                        c += 1
+            if c == 0:
+                ax.plot([j+0.5], [i+0.5], marker='.', color='k')
+
+    ax.set_aspect('equal')
+
+    ax.set_xlim([0, s1])
+    ax.set_ylim([0, s0])
+
+    ax.set_xticks(np.arange(0, s1+1, 4))
+    ax.set_xticks(np.arange(0, s1+1, 1), minor=True)
+    ax.set_yticks(np.arange(0, s0+1, 4))
+    ax.set_yticks(np.arange(0, s0+1, 1), minor=True)
+    ax.grid(axis='both')
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+
+    ax.tick_params(axis='both', which='both',
+                   top=False, bottom=False, labelbottom=False,
+                   left=False, right=False, labelleft=False)
